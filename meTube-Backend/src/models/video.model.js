77acd1346fbccfa1991f9ab4aea499fb89bc530a
sanket-2191 <sanketpadole1@gmail.com>
2025-03-20 16:38:55 +0530

    user and video models

---
+++ meTube-Backend/src/models/video.model.js
@@ -0,0 +1,48 @@
+import { model, Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            unique: true
+        },
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            typetype: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+        },
+        duration: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const videoModel = model('Video', videoSchema);
